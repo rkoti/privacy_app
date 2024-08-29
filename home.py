bc2ebf4f7157aee93938fd11b07e67ab382c7e58
rkoti <roshank08@gmail.com>
2024-08-29 15:20:57 +0200

    Update home.py
    changed the path

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ from streamlit_lottie import st_lottie
 from PIL import Image
 
 
-img_path = r"C:\Users\rkotian\PycharmProjects\Anonymization\tue.png"
+img_path = "/apps/tue.png"
 
 st.set_page_config(
     page_title="Tabular Data Anonymization App",
